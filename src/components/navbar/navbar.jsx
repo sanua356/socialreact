47d958c4077f6a7442876caea1adfe70fe80c23e
sanua356 <sanek.pankratov356@gmail.com>
2021-10-09 18:54:59 +0300

    Сделана мобильная версия страницы диалогов

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -10,15 +10,16 @@ function Navbar(props) {
     const mappedMenuArray = props.sidebarMenuItems.map((menuItem) =>
         <NavLink to={menuItem.link} key={props.sidebarMenuItems.indexOf(menuItem)} activeClassName = {NavbarStyle.activeLink}>
             <li className={NavbarStyle.menuItemStyle}>
-                <span className ={NavbarStyle.menuItemIcon}>
+                <span className={NavbarStyle.menuItemIcon}>
                     <FontAwesomeIcon icon = {menuItem.icon} />
                 </span>
-                <span>{menuItem.itemName}</span>
+                <span className={NavbarStyle.menuItemName}>{menuItem.itemName}</span>
             </li>
         </NavLink>
     );
     return (
         <div className={NavbarStyle.navbar}>
+            <h2 className={NavbarStyle.navbarSiteTitle}>ANONUMOUS CHAT</h2>
             <div className={NavbarStyle.userBlock}>
                 <img src={UsernameIcon} alt="username" />
                 <span>Your name: {props.username}</span>
